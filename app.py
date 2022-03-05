dc380beb63959e8735ba8ee655aa17c0537ae492
Brydon Parker <brydon.parker@shopify.com>
2022-03-05 13:52:08 -0500

    openai example now working in streamlit (pet namer)

--- app.py
+++ app.py
@@ -1,26 +1,12 @@
 import os
-
+from decouple import config
 import openai
-from flask import Flask, redirect, render_template, request, url_for
+import streamlit as st
 
-app = Flask(__name__)
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = config("OPENAI_API_KEY")
 
-
-@app.route("/", methods=("GET", "POST"))
-def index():
-    if request.method == "POST":
-        animal = request.form["animal"]
-        response = openai.Completion.create(
-            engine="text-davinci-001",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
-        )
-        return redirect(url_for("index", result=response.choices[0].text))
-
-    result = request.args.get("result")
-    return render_template("index.html", result=result)
-
+st.title("Name my pet")
+animal = st.text_input("Enter an animal")
 
 def generate_prompt(animal):
     return """Suggest three names for an animal that is a superhero.
@@ -33,3 +19,11 @@ Animal: {}
 Names:""".format(
         animal.capitalize()
     )
+
+response = openai.Completion.create(
+    engine="text-davinci-001",
+    prompt=generate_prompt(animal),
+    temperature=0.6,
+)
+
+st.text(response.choices[0].text)
