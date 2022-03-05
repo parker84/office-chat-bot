fb7eeddb7626114560778c066d6ca6b33f32f943
Brydon Parker <brydon.parker@shopify.com>
2022-03-05 15:40:52 -0500

    adding a sidebar

--- app.py
+++ app.py
@@ -17,6 +17,25 @@ st.set_page_config(
 )
 st.title("🏢 Dunder Mifflin Rabies Awareness Chatbot For the Cure")
 
+st.sidebar.markdown("""
+#### Not sure what to say to Dwight?
+Try some of these:
+```
+1. What is your name?
+2. Where do you work?
+3. What is the best bear?
+4. Michael!
+5. you're fired
+6. beets are the worst vegetable
+```
+
+**Feedback/Questions**: 
+[join our slack workspace](https://join.slack.com/t/officechatbot/shared_invite/zt-14rlr8chh-C~rwJN~~KUAX~DOkvcno1g)
+
+Like 🏢 **The Office Chatbot** and want to say thanks?       
+[:coffee: buy me a coffee](https://www.buymeacoffee.com/brydon)
+""")
+
 
 jim_line = st.text_input("Say something to Dwight Schrute:", value='bears beats battlestar gallactica')
 
@@ -138,17 +157,3 @@ response = openai.Completion.create(
 )
 
 st.text(response.choices[0].text.split('\n')[0])
-
-with st.expander("Not sure what to ask Dwight, try some of these"):
-    st.markdown("""
-    ```
-    1. What is your name?
-    2. Where do you work?
-    3. What is the best bear?
-    4. Michael!
-    5. you're fired
-    6. beets are the worst vegetable
-    ```
-    """)
-
-st.markdown("Feedback/Questions: [join our slack workspace](https://join.slack.com/t/officechatbot/shared_invite/zt-14rlr8chh-C~rwJN~~KUAX~DOkvcno1g)")
